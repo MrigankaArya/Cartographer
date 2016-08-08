49b552e9f675f7f3937468b1d2117fadfff2dd02
MrigankaArya <mrig.arya@gmail.com>
2016-08-08 02:13:10 -0700

    WIP Finding vanishing point

--- good_results.py
+++ good_results.py
@@ -18,6 +18,7 @@ def good_corners():
 
     #make a set to keep track of the coordinates that are detected
     coordinates = []
+    dict_gradient_maps = {}
     
     for i in corners:
         x,y = i.ravel()
@@ -26,39 +27,22 @@ def good_corners():
         point.y = y
         coordinates.append(point)
         cv2.circle(img,(x,y),3,255,-1)
+
+    while len(coordinates ) > 2 :
+        smallest = find_smallest(coordinates)
+        #if it's been part of a gradient check and the gradient coordinate list
+        #is a good size, then the coordinates are removed from the list - they
+        #don't belong in there anymore
+        #we dont want to check the gradient for something we're checking against
+        coordinates.remove(smallest)
+        dict_gradient_maps[smallest] = gradients(coordinates)
         
-    smallest = find_smallest(coordinates)
-    #make a duplicate list to keep track of which coordinates
-    #have already been calculated for a gradient check
-    #if it's been part of a gradient check and the gradient coordinate list
-    #is a good size, then the coordinates are removed from the list - they
-    #don't belong in there anymore
-
-    checklist = list(coordinates)
-    #we dont want to check the gradient for something we're checking against
-    checklist.remove(smallest)
-
-    #hashmap functionality to hold the points sharing the same list of gradients
-    #wrt the smallest point
-    gradient_map = {}
-    list_points = []
-
-    gradient = 0
-    for p in checklist:
-        if p.x == smallest.x:
-            continue
-
-        gradient = round((p.y - smallest.y)/(p.x - smallest.x),2)
-
-        if gradient in gradient_map:
-            gradient_map[gradient].append(p)
-        else:
-            gradient_map[gradient] = [p]
-    
+    dict_gradient_maps = {key: value for key, value in dict_gradient_maps.items() if len(value) > 0}    
     plt.imshow(img),plt.show()
+    print dict_gradient_maps
 
 
- #find the smallest x value for the 1st step of the algorithm
+#find the smallest x value for the 1st step of the algorithm
 def find_smallest(coordinates):
     smallest = coordinates[0]
     place = 0
@@ -68,6 +52,27 @@ def find_smallest(coordinates):
             place = point
             smallest = coordinates[point]
     return smallest
- 
+
+
+def gradients(checklist):
+    gradient_map = {}
+    smallest = find_smallest(checklist)
+    gradient = 0
+    for p in checklist:
+        if p.x == smallest.x:
+            continue
+
+        gradient = round((p.y - smallest.y)/(p.x - smallest.x),1)
+
+        if gradient in gradient_map:
+            gradient_map[gradient].append(p)
+        else:
+            gradient_map[gradient] = [p]
+
+    print gradient_map
+    #Removing gradients with only 1 point in the thingy
+    condensed_map = {key: value for key, value in gradient_map.items() if len(value) > 1}
+    print "----------------------------------------------"
+    return condensed_map
 
 good_corners()
